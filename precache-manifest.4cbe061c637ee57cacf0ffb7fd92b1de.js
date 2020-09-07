0694b0ecda5daf223c7e74f4203b9ca6695a903c
Chris Noty <ChrisNoty@MacBook-Pro.attlocal.net>
2020-09-07 10:21:14 -0500

    Updates

---
+++ precache-manifest.4cbe061c637ee57cacf0ffb7fd92b1de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a2665cea57c30563297db5793cf712",
+    "url": "/summersalt-interview/index.html"
+  },
+  {
+    "revision": "72d047d6c2319d3a64fe",
+    "url": "/summersalt-interview/static/css/main.8ab235b3.chunk.css"
+  },
+  {
+    "revision": "037075cb39c57be8979a",
+    "url": "/summersalt-interview/static/js/2.f26aa3d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/summersalt-interview/static/js/2.f26aa3d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72d047d6c2319d3a64fe",
+    "url": "/summersalt-interview/static/js/main.2ed1d953.chunk.js"
+  },
+  {
+    "revision": "9452fdf8c9dbf5af7e22",
+    "url": "/summersalt-interview/static/js/runtime-main.8d7a4cac.js"
+  }
+]);
